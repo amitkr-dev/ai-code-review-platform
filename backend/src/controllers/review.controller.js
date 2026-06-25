4dc7ae3df37886e8fc125895f7a747089256a78c
Amit kumar <amitkumardev624@gmail.com>
2026-06-25 20:25:42 +0530

    fix: correct case of imported groq.service module

--- backend/src/controllers/review.controller.js
+++ backend/src/controllers/review.controller.js
@@ -13,7 +13,7 @@ console.log(
   Review.schema.path("codeSmells")
 );
 const User = require("../models/User.model");
-const { analyzeCode, streamReview } = require("../utils/Groq.service");
+const { analyzeCode, streamReview } = require("../utils/groq.service");
 
 /**
  * POST /api/reviews
